85652b1bf617d02db2b5aa9b73d2dda2889a71d7
Nanda-Aung <aye81061@gmail.com>
2022-11-08 16:14:26 +0630

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="cols">
+        <v-card>
+          <v-card-title>Dashboard</v-card-title>
+        </v-card>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'dashboard'
+  // middleware: 'auth'
+}
+</script>
